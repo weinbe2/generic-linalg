bf5adade2b477e597ffaf4d1e1ad8bd19298be23
Evan Weinberg <weinbe2@bu.edu>
2016-08-24 18:24:04 -0400

    Turned verbosity into just a header with function defines.

--- verbosity.h
+++ verbosity.h
@@ -3,6 +3,8 @@
 #ifndef VERBOSITY
 #define VERBOSITY
 
+#include <iostream>
+#include <string>
 
 enum inversion_verbose_level
 {
@@ -21,19 +23,90 @@ struct inversion_verbose_struct
     std::string precond_verb_prefix; // Prefix for preconditioner. 
 };
 
-// Shuffles preconditioned values into current values. 
-void shuffle_verbosity_precond(inversion_verbose_struct* verb_new, inversion_verbose_struct* verb);
+// Shuffles restarted values into current values. 
+inline void shuffle_verbosity_restart(inversion_verbose_struct* verb_new, inversion_verbose_struct* verb)
+{
+    if (verb != 0)
+    {
+        verb_new->verbosity = (verb->verbosity == VERB_RESTART_DETAIL || verb->verbosity == VERB_SUMMARY) ? VERB_NONE : verb->verbosity; 
+        verb_new->verb_prefix = verb->verb_prefix;
+        verb_new->precond_verbosity = verb->precond_verbosity;
+        verb_new->precond_verb_prefix = verb->precond_verb_prefix; 
+    }
+    else
+    {
+        verb_new->verbosity = VERB_NONE;
+        verb_new->verb_prefix = "";
+        verb_new->precond_verbosity = VERB_NONE;
+        verb_new->precond_verb_prefix = "";
+    }
+}
 
-// Shuffles values for restarted solves.
-void shuffle_verbosity_restart(inversion_verbose_struct* verb_new, inversion_verbose_struct* verb);
+// Shuffles values for preconditioned solves.
+inline void shuffle_verbosity_precond(inversion_verbose_struct* verb_new, inversion_verbose_struct* verb)
+{
+    if (verb != 0)
+    {
+        if (verb->precond_verbosity == VERB_PASS_THROUGH)
+        {
+            verb_new->verbosity = verb->verbosity;
+        }
+        else
+        {
+            verb_new->verbosity = verb->precond_verbosity;
+        }
+        verb_new->verb_prefix = verb->precond_verb_prefix;
+        verb_new->precond_verbosity = VERB_NONE;
+        verb_new->precond_verb_prefix = "";
+    }
+    else
+    {
+        verb_new->verbosity = VERB_NONE;
+        verb_new->verb_prefix = "";
+        verb_new->precond_verbosity = VERB_NONE;
+        verb_new->precond_verb_prefix = "";
+    }
+    
+}
 
 // Properly prints relative residual.
-void print_verbosity_resid(inversion_verbose_struct* verb, std::string alg, int iter, double relres); 
+inline void print_verbosity_resid(inversion_verbose_struct* verb, std::string alg, int iter, double relres)
+{
+    if (verb != 0)
+    {
+        if (verb->verbosity == VERB_DETAIL)
+        {
+            std::cout << verb->verb_prefix << alg << " Iter " << iter << " RelRes " << relres << "\n";
+        }
+    }
+    
+    return; 
+}
 
 // Properly prints summary at end of inversion.
-void print_verbosity_summary(inversion_verbose_struct* verb, std::string alg, bool success, int iter, double relres); 
+inline void print_verbosity_summary(inversion_verbose_struct* verb, std::string alg, bool success, int iter, double relres)
+{
+    if (verb != 0)
+    {
+        if (verb->verbosity == VERB_SUMMARY || verb->verbosity == VERB_RESTART_DETAIL || verb->verbosity == VERB_DETAIL)
+        {
+            std::cout << verb->verb_prefix << alg << " Success " << (success ? "Y" : "N") << " Iter " << iter << " RelRes " << relres << "\n";
+        }
+    }
+    
+    return; 
+}
 
 // Properly prints summary at restart.
-void print_verbosity_restart(inversion_verbose_struct* verb, std::string alg, int iter, double relres); 
+inline void print_verbosity_restart(inversion_verbose_struct* verb, std::string alg, int iter, double relres)
+{
+    if (verb != 0)
+    {
+        if (verb->verbosity == VERB_RESTART_DETAIL || verb->verbosity == VERB_DETAIL)
+        {
+            std::cout << verb->verb_prefix << alg << " Iter " << iter << " RelRes " << relres << "\n";
+        }
+    }
+}
 
 #endif // VERBOSITY
