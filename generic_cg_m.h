dafad846da71d315bf3e4ed69608fcd0ac72d8c4
Evan Weinberg <weinbe2@bu.edu>
2016-09-26 00:49:18 -0400

    Implemented CG-M for double. Coded up test routine.

---
+++ generic_cg_m.h
@@ -0,0 +1,21 @@
+// Wed Aug 24 15:46:50 EDT 2016
+// Evan S Weinberg
+// Include file for CG-M
+
+#ifndef ESW_INVERTER_CG_M
+#define ESW_INVERTER_CG_M
+
+#include <string>
+#include <complex>
+using std::complex;
+
+#include "inverter_struct.h"
+#include "verbosity.h"
+
+// Multishift CG defined in http://arxiv.org/pdf/hep-lat/9612014.pdf
+inversion_info minv_vector_cg_m(double **phi, double *phi0, int n_shift, int size, int max_iter, double eps, double* shifts, void (*matrix_vector)(double*,double*,void*), void* extra_info, inversion_verbose_struct* verbosity = 0);
+//inversion_info minv_vector_cg(complex<double>  *phi, complex<double>  *phi0, int size, int max_iter, double res, void (*matrix_vector)(complex<double>*,complex<double>*,void*), void* extra_info, inversion_verbose_struct* verbosity = 0);
+
+// multishift CG starts with 0 initial guess, so a restarted version wouldn't work. 
+
+#endif
--- inverter_struct.h
+++ inverter_struct.h
@@ -5,6 +5,9 @@
 #ifndef ESW_INVERTER_STRUCT
 #define ESW_INVERTER_STRUCT
 
+#include <iostream>
+using namespace std;
+
 // Struct that contains information that
 // all matrix functions return.
 // Return for various matrix functions.
@@ -26,13 +29,13 @@ struct inversion_info
   // Multirhs constructor
   inversion_info(int in_n_rhs) : resSq(0.0), iter(0), success(false), name(""), ops_count(0), resSqmrhs(0), n_rhs(in_n_rhs)
   {
-    resSqmrhs = new double[n_rhs];
+    resSqmrhs = new double[in_n_rhs];
   }
   
   // Destructor.
   ~inversion_info()
   {
-    if (resSqmrhs != 0)
+    if (resSqmrhs != 0 && n_rhs > 0)
     {
       delete[] resSqmrhs;
       resSqmrhs = 0;
@@ -49,7 +52,7 @@ struct inversion_info
     ops_count = obj.ops_count;
     
     n_rhs = obj.n_rhs;
-    if (resSqmrhs != 0)
+    if (obj.resSqmrhs != 0 && n_rhs > 0)
     {
       resSqmrhs = new double[n_rhs];
       for (int i = 0; i < n_rhs; i++)
@@ -62,6 +65,27 @@ struct inversion_info
       resSqmrhs = 0;
     }
   }
+  
+  // Assignment operator.
+  inversion_info& operator=(inversion_info obj) // Pass by value (thus generating a copy)
+  {
+    resSq = obj.resSq;
+    iter = obj.iter;
+    success = obj.success;
+    name = obj.name;
+    ops_count = obj.ops_count;
+    
+    n_rhs = obj.n_rhs;
+    if (obj.resSqmrhs != 0 && n_rhs > 0)
+    {
+      resSqmrhs = obj.resSqmrhs;
+      obj.resSqmrhs = 0;
+    }
+    return *this;
+  }
+  
+  // For C++11, may need to add move constructors?
+  // http://stackoverflow.com/questions/255612/dynamically-allocating-an-array-of-objects
 };
 
 #endif
